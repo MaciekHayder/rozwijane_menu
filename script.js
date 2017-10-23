05f5085dd9318160551419fefcd9c987bddbbf45
machayder <33019800+machayder@users.noreply.github.com>
2017-10-23 11:03:31 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+$("document").ready(
+	function()
+	{
+		$("div.naglowek span").each(
+			function(i)
+			{			
+				$(this).addClass("gh"+i);
+			}
+		);
+		$("div.ukryty_tekst").each(
+			function(i)
+			{			
+				$(this).addClass("gh"+i);
+			}
+		);		
+		
+		
+		$("div.naglowek span").click(
+			function()
+			{
+				$("div."+$(this).attr("class")).toggle(400);
+			}
+		
+		).toggle(
+			function()
+			{
+				$(this).text("schowaj");
+			},
+			function()
+			{
+				$(this).text("więcej");
+			}
+		
+		);
+	
+	}
+);
+
